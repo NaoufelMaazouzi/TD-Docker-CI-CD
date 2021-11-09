4b3a97d01ceb66d5258caed31991485f295acdf2
Naoufel <naoufel.maazouzi@live.fr>
2021-11-09 12:28:02 +0100

    add test title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TD Docker CI CD Naoufel
+          TEST
         </p>
         <a
           className="App-link"
