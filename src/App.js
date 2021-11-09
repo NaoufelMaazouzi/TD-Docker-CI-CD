90b232f0b09ecd89ac3927578045b9d3faebc892
Naoufel <naoufel.maazouzi@live.fr>
2021-11-09 15:44:33 +0100

    remove code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TD Docker CI CD Naoufel
+          TD Docker CI CD
         </p>
         <a
           className="App-link"
