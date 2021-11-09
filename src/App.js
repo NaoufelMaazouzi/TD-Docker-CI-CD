a62e6e3609ed3e35ac1322ab9cf7f09c3235eedf
Naoufel <naoufel.maazouzi@live.fr>
2021-11-09 15:05:23 +0100

    test branch to develop

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST
+          TEST Develop
         </p>
         <a
           className="App-link"
