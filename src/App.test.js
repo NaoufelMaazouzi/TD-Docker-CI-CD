a1c6504853206acb970b13d58aa0b53260c31efe
Naoufel <naoufel.maazouzi@live.fr>
2021-11-09 12:06:19 +0100

    add circleci config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders TD Docker CI CD Naoufel link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/TD Docker CI CD Naoufel/i);
   expect(linkElement).toBeInTheDocument();
 });
